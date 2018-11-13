4c6cc2991bc7fbbd030d7177cf717819dbf9b2f4
Fatih Guclu <fatihguclu40@hotmail.com>
2018-11-13 22:05:57 +0300

    sil

--- resources/views/backend/silinecek.blade.php
+++ resources/views/backend/silinecek.blade.php
@@ -1,22 +1,6 @@
 @extends('')
 @section('icerik')
 asdasdasdasd
-    asdasdasd
-    asdasd
-    asd
-    asd
-    asd
-    asd
-    asdasdasdasd
-    asd
-    asd
-    asd
-    asd
-    asd
-    asd
-    asd
-    asd
-    asd
 @endsection
 
 @section('css')
